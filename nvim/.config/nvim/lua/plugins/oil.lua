ad479bbe96cdb31ea43bd9eb465b376c3691e336
pbootly <mthomas@polybit.io>
2025-08-25 00:38:48 +0100

    disable noice

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,42 @@
 return {
-  "stevearc/oil.nvim",
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+          return name:match("^%.") ~= nil
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+          return false
+        end,
+        -- Sort file names with numbers in a more intuitive order for humans
+        natural_order = "fast",
+        -- Sort file and directory names case insensitive
+        case_insensitive = false,
+        sort = {
+          { "type", "asc" },
+          { "name", "asc" },
+        },
+      },
+    },
+    dependencies = {
+      { "echasnovski/mini.icons", opts = {} },
+    },
+    lazy = false,
+    keys = {
+      {
+        "<leader>fo",
+        function()
+          require("oil").open()
+        end,
+        desc = "Open Oil (file explorer)",
+      },
+    },
+  },
 }
