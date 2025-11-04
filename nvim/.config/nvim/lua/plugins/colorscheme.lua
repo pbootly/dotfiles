b8a1a87730a65b7dea94c86cee6ba3259e5526b4
Matthew Thomas <mthomas@polybit.io>
2025-11-04 17:27:45 +0000

    colorsceme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,16 @@
 return {
+  -- {
+  --   "EdenEast/nightfox.nvim",
+  --   config = function()
+  --     vim.cmd([[colorscheme terafox]])
+  --   end,
+  -- },
   {
-    "EdenEast/nightfox.nvim",
+    "zootedb0t/citruszest.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-      vim.cmd([[colorscheme terafox]])
+      vim.cmd([[colorscheme citruszest]])
     end,
   },
 }
--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,19 +5,14 @@ return {
     ---@type oil.SetupOpts
     opts = {
       view_options = {
-        -- Show files and directories that start with "."
         show_hidden = true,
-        -- This function defines what is considered a "hidden" file
         is_hidden_file = function(name, bufnr)
           return name:match("^%.") ~= nil
         end,
-        -- This function defines what will never be shown, even when `show_hidden` is set
         is_always_hidden = function(name, bufnr)
           return false
         end,
-        -- Sort file names with numbers in a more intuitive order for humans
         natural_order = "fast",
-        -- Sort file and directory names case insensitive
         case_insensitive = false,
         sort = {
           { "type", "asc" },
@@ -26,12 +21,12 @@ return {
       },
     },
     dependencies = {
-      { "echasnovski/mini.icons", opts = {} },
+      { "nvim-mini/mini.icons", opts = {} },
     },
     lazy = false,
     keys = {
       {
-        "<leader>fo",
+        "-",
         function()
           require("oil").open()
         end,
